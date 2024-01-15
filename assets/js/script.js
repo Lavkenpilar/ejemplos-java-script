599fa66fc51b76bf703e2a971a356bc330dc50d8
Lavken <lavkenpilar@gmail.com>
2024-01-15 16:20:57 -0300

    create-archivo

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+console.log ("estoy cargando desde mi archivo")
+
+const nombreApellido = "Mi nombre es Lavken Pilar Olave Alvarado"
+const edad_de_lavken = 40
+const verdadero = true
+
+console.log (nombreApellido)
+console.log (edad_de_lavken)
+
+function nombre_persona (nombre) {
+    console.log ("hola soy la funcion creada")
+    console.log (nombre)
+}
+
+nombre_persona ("pilar")
+let button = document.getElementById ("#mari")
+console.log (mari)
+/*mari.addEventListener ("click", function () {
+    alert ("Mari mari kom pu che")
+}
+) */
+mari.addEventListener ("click", function () {
+    document.querySelector ("h1").innerHTML="Titulo Nuevo"
+}
+)
